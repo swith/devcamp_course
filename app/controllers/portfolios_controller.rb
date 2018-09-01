72e92836eeca89f829d06c5d76daf9b7f020f7b3
Alexey Zvezdochkin <swith@tetrasis.ru>
2018-09-01 16:14:14 +0300

    added form, new, create

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,27 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html {redirect_to portfolios_path, notice: 'Created'}
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :body, :subtitle)
+  end
+
 end
