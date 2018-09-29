ffaa3da1c95ce0a4329ff4ceb12241bfb1bf85d1
Alexey Zvezdochkin <swith@tetrasis.ru>
2018-09-29 20:53:34 +0300

    gritter refactored

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -55,4 +55,15 @@ module ApplicationHelper
   def active?(path)
     "active" if current_page?(path)
   end
+
+  def alerts
+    alert = (flash[:alert] || flash[:notice] || flash[:error])
+    if alert
+      alert_generator(alert)
+    end
+  end
+
+  def alert_generator(msg)
+    js add_gritter(msg, title: "Alex Portfolio", sticky: false, time: 1500)
+  end
 end
